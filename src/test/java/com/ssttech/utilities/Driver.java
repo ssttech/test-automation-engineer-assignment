fd7506d47999f94f9f15117e9982efce09fc7ea7
ARIKBOGA <barikboga42@gmail.com>
2025-01-03 01:04:04 +0300

    RemoteWebDriver option has been added into Driver class

--- src/test/java/com/ssttech/utilities/Driver.java
+++ src/test/java/com/ssttech/utilities/Driver.java
@@ -4,91 +4,132 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.Browser;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.Collections;
-import java.util.concurrent.TimeUnit;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
 
-/**
- * Making abstract this class and implementing encapsulation, so we are closing
- * access to the object of this class from outside the class
- */
-public abstract class Driver {
+public class Driver {
+
+    private static final String BROWSER = System.getProperty("browser") != null
+            ? System.getProperty("browser")
+            : ConfigurationReader.getProperty("browser");
+
+    private static final InheritableThreadLocal<WebDriver> DRIVER_POOL = new InheritableThreadLocal<>();
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(Driver.class);
+
+    private Driver() {
+    }
 
     /**
-     * We make WebDriver private, because we want to close access from outside the class.
-     * We make it static because we will use it in a static method.
-     */
-    //private static WebDriver driver; // value is null by default
-
-    private static final InheritableThreadLocal<WebDriver> driverPool = new InheritableThreadLocal<>();
-
-    /**
-     * Create a re-usable utility method which will return same driver instance when we call it
+     * Retrieves the WebDriver instance from the driver pool.
+     * If no instance is available, a new one is created and set in the pool.
+     *
+     * @return the WebDriver instance for the current thread
      */
     public static WebDriver getDriver() {
+        // Check if the driver pool is empty for the current thread
+        if (DRIVER_POOL.get() == null) {
+            // Create and set a new WebDriver instance in the pool
+            DRIVER_POOL.set(createDriver());
+        }
+        // Return the WebDriver instance from the pool
+        return DRIVER_POOL.get();
+    }
 
-        if (driverPool.get() == null) {
+    /**
+     * Creates a WebDriver instance based on the specified browser type.
+     * The browser type is read from the configuration properties.
+     *
+     * @return a WebDriver instance configured for the specified browser
+     */
+    private static WebDriver createDriver() {
+        LOGGER.info("Thread ID: {}, Starting Driver initialization for browser: {}", Thread.currentThread().getId(), BROWSER);
 
-            /*
-            We read our browserType from configuration.properties.
-            This way, we can control which browser is opened from outside our code, from configuration.properties.
-             */
-            String browserType = ConfigurationReader.getProperty("browser");
-
-            /*
-                Depending on the browserType that will be return from configuration.properties file
-                switch statement will determine the case, and open the matching browser
-            */
-            switch (browserType) {
-                case "chrome":
-                    WebDriverManager.chromedriver().setup();
-                    driverPool.set(new ChromeDriver(getChromeOptions(false, "disable-infobars", "disable-popup-blocking", "start-maximized")));
-                    break;
-                case "firefox":
-                    WebDriverManager.firefoxdriver().setup();
-                    driverPool.set(new FirefoxDriver());
-                    break;
-                case "safari":
-                    WebDriverManager.safaridriver().setup();
-                    driverPool.set(new SafariDriver());
-                    break;
-                case "edge":
-                    WebDriverManager.edgedriver().setup();
-                    driverPool.set(new EdgeDriver());
-                    break;
+        // Determine which WebDriver to initialize based on the browser type
+        return switch (BROWSER.toLowerCase()) {
+            case "remote" -> setupRemoteDriver(); // Setup a remote WebDriver for grid execution
+            case "chrome" -> setupChromeDriver(); // Setup a local Chrome WebDriver
+            case "firefox" -> setupFirefoxDriver(); // Setup a local Firefox WebDriver
+            case "safari" -> setupSafariDriver(); // Setup a local Safari WebDriver
+            default -> {
+                LOGGER.error("Unknown browser type : {}", BROWSER);
+                // Throw an exception for unsupported browser types
+                throw new IllegalArgumentException("Unknown browser type : " + BROWSER);
             }
-            driverPool.get().manage().window().maximize();
-            driverPool.get().manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-        }
-
-        return driverPool.get();
-
+        };
     }
 
     /**
-     * This method returns chromeOptions after specified
+     * Create a remote {@link WebDriver} instance to interact with the grid.
+     *
+     * @return a remote {@link WebDriver} instance
      */
-    private static ChromeOptions getChromeOptions(boolean isHeadless, String... args) {
+    private static WebDriver setupRemoteDriver() {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName(Browser.CHROME.browserName());
+
+        // Specify the URL of the grid hub
+        URL url;
+        try {
+            url = new URL("http://localhost:4444/wd/hub");
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Grid URL is invalid!", e);
+        }
+        // Create the remote driver
+        return new RemoteWebDriver(url, capabilities);
+    }
+
+
+    private static WebDriver setupChromeDriver() {
+        WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
-        options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
-        options.setExperimentalOption("useAutomationExtension", false);
-        options.addArguments(args);
-        if (isHeadless) {
-            options.addArguments("--headless");
+        String chromeOptions = ConfigurationReader.getProperty("chrome.options");
+        if (chromeOptions != null && !chromeOptions.isEmpty()) {
+            options.addArguments(chromeOptions.split(","));
         }
-        return options;
+        options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
+        options.setExperimentalOption("useAutomationExtension", false);
+        return new ChromeDriver(options);
     }
 
-    /**
-     * This method will make sure our driver value is always null after using quit() method
-     */
+    private static WebDriver setupFirefoxDriver() {
+        WebDriverManager.firefoxdriver().setup();
+        FirefoxOptions options = new FirefoxOptions();
+        String firefoxOptions = ConfigurationReader.getProperty("firefox.options");
+        if (firefoxOptions != null && !firefoxOptions.isEmpty()) {
+            options.addArguments(firefoxOptions.split(","));
+        }
+        return new FirefoxDriver(options);
+    }
+
+    private static WebDriver setupSafariDriver() {
+        return new SafariDriver();
+    }
+
+    private static void setDriverDefaults(WebDriver driver) {
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
     public static void closeDriver() {
-        if (driverPool.get() != null) {
-            driverPool.get().quit(); // this line will terminate the existing session. value will not even be null
-            driverPool.remove();
+        if (DRIVER_POOL.get() != null) {
+            try {
+                DRIVER_POOL.get().quit();
+            } catch (Exception e) {
+                LOGGER.error("Error occurred while closing the WebDriver instance", e);
+            } finally {
+                DRIVER_POOL.remove();
+            }
         }
     }
 }
